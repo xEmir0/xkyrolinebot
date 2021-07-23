1b6e64b3c988e4b9f0cfeda08e073cc627c5fb80
xEmir0 <85646334+xEmir0@users.noreply.github.com>
2021-07-23 22:57:47 +0300

    Update sunucubilgi.js

--- komutlar/sunucubilgi.js
+++ komutlar/sunucubilgi.js
@@ -1,38 +1,41 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+
 module.exports = {
-    kod:"sunucu-bilgi",
-    async run (client, message, args) {
-        const { MessageEmbed } = require('discord.js')
-        var guild = message.guild
-        var kanallar = guild.channels.cache
-        var üyeler = guild.members.cache
-        var emojiler = guild.emojis.cache
-        var roller = guild.roles.cache
-        var embed = new MessageEmbed()
-        .setTitle(`${guild.name} Adlı Sunucunun Bilgileri`)
-        .addField('**Temel Bilgiler**', [
-            `**Sunucu Adı:** ${guild.name}`,
-            `**Sunucu ID'si:** ${guild.id}`,
-            `**Sunucu Sahibi:** <@${guild.owner.id}>`,
-            `**Sunucu Sahibinin Tagı:** ${guild.owner.user.tag}`
-        ])
-        .addField(`**İSTATİSTİK**`, [
-            `**Üye Sayısı:** ${guild.memberCount}`,
-            `**Bot Olmayan Kişi Sayısı:** ${üyeler.filter(üye => !üye.user.bot).size}`,
-            `**Bot Sayısı:** ${üyeler.filter(üye => üye.user.bot).size}`,
-            `**Emoji Sayısı:** ${emojiler.size}`,
-            `**Rol Sayısı:** ${roller.size}`,
-            `**Kanal Sayısı:** ${kanallar.size}`,
-            `**Metin Kanalları Sayısı:** ${kanallar.filter(kanal => kanal.type === 'text').size}`,
-            `**Ses Kanalları Sayısı:** ${kanallar.filter(kanal => kanal.type === 'voice').size}`,
-            `**Kategori Sayısı:** ${kanallar.filter(kanal => kanal.type === 'category').size}`,
-        ])
-        .addField('**DURUMLAR**', [
-            `**Çevrimiçi:** ${üyeler.filter(üye => üye.presence.status === 'online').size}`,
-            `**Boşta:** ${üyeler.filter(üye => üye.presence.status === 'idle').size}`,
-            `**Rahatsız Etme:** ${üyeler.filter(üye => üye.presence.status === 'dnd').size}`,
-            `**Çevrimdışı:** ${üyeler.filter(üye => üye.presence.status === 'offline').size}`,
-        ])
-        .setTimestamp(guild.created.Timestamp)
-        message.channel.send(embed)
+    kod: "sunucu-bilgi",
+    async run (client, message, params) {
+        if (!message.guild) {
+    
+            const ozelmesajuyari = new Discord.MessageEmbed()
+            
+            .setColor("RED")
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .addField("Uyarı","Bu Komutu Özel Mesajlarda Kullanamazsın!");
+              
+            return message.author.send(ozelmesajuyari);
+            }
+            
+            if (message.channel.type !== "dm") {
+              
+            const guild = message.guild.id
+            
+            const wynex = new Discord.MessageEmbed()
+              
+            .setColor("GREEN")
+              
+            .setAuthor(message.guild.name, message.guild.iconURL)
+              
+            .addField("Sunucu Sahibi", message.guild.owner)
+            .addField('Sunucu İsmi', message.guild.name)
+            .addField('Sunucu İdsi', message.guild.id)
+            .addField('Sunucu Bölgesi', message.guild.region)
+            .addField('Üye Sayısı', message.guild.memberCount)
+            .addField('Oluşturma tarihi:', message.guild.createdAt, true)
+              
+            .setThumbnail(message.guild.iconURL);
+              
+            return message.channel.send(wynex)
+            }
+            
+            }
     }
-}
