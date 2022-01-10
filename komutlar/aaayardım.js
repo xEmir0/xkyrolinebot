52b93904a0ac086b458ed435e5fe368d8050e470
xEmir0 <85646334+xEmir0@users.noreply.github.com>
2022-01-10 22:45:55 +0300

    Update aaayardım.js

--- komutlar/aaayardım.js
+++ komutlar/aaayardım.js
@@ -10,7 +10,7 @@ module.exports = {
     .addField('??emoji', 'Emoji Komutları',)
     .addField('??eğlence', 'Eğlence Komutları',)
     .addField('??diğer', 'Diğer Komutlar',)
-    .addField('**BAĞLANTILAR**', '[Davet Et](https://discord.com/api/oauth2/authorize?client_id=930137214283812874&permissions=8&scope=bot)')
+    .addField('**BAĞLANTILAR**', '[Davet Et](https://discord.com/api/oauth2/authorize?client_id=930137214283812874&permissions=8&scope=bot)/'[Davet Et](https://discord.com/api/oauth2/authorize?client_id=930137214283812874&permissions=8&scope=bot)')
     message.channel.send(embed)
 
   }
